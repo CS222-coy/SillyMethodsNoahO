069bfc1d645d398c9852bf214e99677fbcab6ad3
Noah Oosterhoff <noah.oosterhoff@bsu.edu>
2022-09-06 12:58:58 -0400

    Initial commit did not have classes, added them in this commit

---
+++ src/main/java/edu/bsu/cs222/SillyMethods.java
@@ -0,0 +1,19 @@
+package edu.bsu.cs222;
+
+public class SillyMethods {
+
+    public boolean didGuess42(int i) {
+        if(i == 42) {
+            return true;
+        }
+        return false;
+    }
+
+    public String countTo(int i) {
+        String numberCount = "0";
+        for(int x = 1; x <= i; x++) {
+            numberCount += (" " + x);
+        }
+        return numberCount;
+    }
+}
